3cbffed10ead0ad4398d5f9c7402a6e09cee7c3d
Colin Watson <cjwatson@canonical.com>
2019-08-11 18:25:01 +0100

    Handle exception text changes in Python 3.3.

--- tests/schema/patch.py
+++ tests/schema/patch.py
@@ -354,7 +354,10 @@ class PatchApplierTest(MockerTestCase):
             self.patch_applier.apply_all()
         except BadPatchError as e:
             self.assertTrue("mypackage/patch_999.py" in str(e))
-            self.assertTrue("takes no arguments" in str(e))
+            if sys.version_info >= (3, 3):
+                self.assertTrue("takes 0 positional arguments" in str(e))
+            else:
+                self.assertTrue("takes no arguments" in str(e))
             self.assertTrue("TypeError" in str(e))
         else:
             self.fail("BadPatchError not raised")
